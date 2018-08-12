16adf5205411b8b47f58f12841d6702455598cda
Jesus Ramirez <jesus.ramirez@GDLA-MC-180723B.local>
2018-08-11 22:39:11 -0500

    Firts commit

---
+++ Magic 8 ball/Magic 8 ball/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by Jesus Ramirez on 8/11/18.
+//  Copyright © 2018 Jesus Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newBallImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic 8 ball
+
+Run your app and ask your Magic 8 Ball to predict the future :)
